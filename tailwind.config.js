d112f6ce41ed9469d744fc4d7e190b811be7c4e8
comby quentin <quentinc33@yahoo.com>
2024-07-12 12:27:28 +0200

    changement nom de page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.*"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'baskerville': ['"Libre Baskerville"', 'serif'],
   },
   plugins: [],
+  }
 }
-
